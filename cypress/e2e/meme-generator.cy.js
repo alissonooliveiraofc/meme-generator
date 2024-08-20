01a66d82051c9765adbe27e7044ea3cfeddb6daf
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2024-08-20 02:46:10 -0300

    comitão bomba feat gus

--- cypress/e2e/meme-generator.cy.js
+++ cypress/e2e/meme-generator.cy.js
@@ -7,12 +7,14 @@ const evaluateOffset = (doc, selector, offsetType) => {
 const isOnSameHorizontal = (firstElement, secondElement) =>
   (firstElement.top >= secondElement.top &&
     firstElement.bottom <= secondElement.bottom) ||
-  (secondElement.top >= firstElement.top && secondElement.bottom <= firstElement.bottom);
+  (secondElement.top >= firstElement.top &&
+    secondElement.bottom <= firstElement.bottom);
 
 const isOnSameVertical = (firstElement, secondElement) =>
   (firstElement.left >= secondElement.left &&
     firstElement.right <= secondElement.right) ||
-  (secondElement.left >= firstElement.left && secondElement.right <= firstElement.right);
+  (secondElement.left >= firstElement.left &&
+    secondElement.right <= firstElement.right);
 
 const isOverEachother = (backgroundElement, forefrontElement) => {
   backgroundElement.bottom = backgroundElement.top + backgroundElement.height;
@@ -28,7 +30,7 @@ const isOverEachother = (backgroundElement, forefrontElement) => {
 };
 
 const checkFullOverlappage = (backgroundSelector, forefrontSelector) => {
-  cy.document().then(doc => {
+  cy.document().then((doc) => {
     const backgroundElement = {
       top: evaluateOffset(doc, backgroundSelector, 'top'),
       height: evaluateOffset(doc, backgroundSelector, 'height'),
@@ -64,16 +66,16 @@ const typeText = () => {
   cy.get('#text-input')
     .type('My awesome meme')
     .should('have.value', 'My awesome meme');
-}
+};
 
 const checkTypedTextIsVisible = () => {
   cy.contains(/^My awesome meme$/).should('be.visible');
-}
+};
 
 const memeUpload = () => {
   const fileName = 'meme.jpeg';
-  cy.fixture(fileName).then(fileContent => {
-    cy.get('#meme-insert').upload({
+  cy.fixture(fileName).then((fileContent) => {
+    cy.get('#meme-insert').attachFile({
       fileContent,
       fileName,
       mimeType: 'image/jpeg',
@@ -83,184 +85,167 @@ const memeUpload = () => {
 
 //-------
 
-describe("1 - Crie um input de texto", () => {
+describe('1 - Crie um input de texto', () => {
   beforeEach(() => {
     cy.viewport(1366, 768);
     cy.visit('/');
-  })
+  });
 
-  it("Será validado se o input de texto existe e conseguimos inputar texto nele", () => {
+  it('Será validado se o input de texto existe e conseguimos inputar texto nele', () => {
     typeText();
-  })
+  });
 
-  it("Será validado se o texto digitado no input é visível na tela", () => {
+  it('Será validado se o texto digitado no input é visível na tela', () => {
     typeText();
     checkTypedTextIsVisible();
-  })
+  });
 
-  it("Será validado se existe um elemento de container para onde o texto será mostrado", () => {
+  it('Será validado se existe um elemento de container para onde o texto será mostrado', () => {
     typeText();
     checkTypedTextIsVisible();
     checkFullOverlappage('#meme-image-container', '#meme-text');
-  })
+  });
 });
 
-describe("2 - Crie uma função que execute upload de imagem", () => {
+describe('2 - Crie uma função que execute upload de imagem', () => {
   beforeEach(() => {
     cy.viewport(1366, 768);
     cy.visit('/');
-  })
+  });
 
-  it("Será validado se é possível carregar uma imagem através do elemento correto", () => {
+  it('Será validado se é possível carregar uma imagem através do elemento correto', () => {
     memeUpload();
-  })
+  });
 
-  it("Será validado se a imagem carregada é exibida dentro do elemento correto", () => {
+  it('Será validado se a imagem carregada é exibida dentro do elemento correto', () => {
     memeUpload();
     cy.get('#meme-image').should('be.visible');
-  })
+  });
 
-  it("Será validado se o texto é inserido corretamente sobre a imagem", () => {
+  it('Será validado se o texto é inserido corretamente sobre a imagem', () => {
     memeUpload();
     cy.get('#meme-image').should('be.visible');
     typeTextAndCheckItsPosition('#meme-image', '#meme-text');
-  })
+  });
 });
 
-describe("3 - Adicione uma moldura no preview de visualização", () => {
+describe('3 - Adicione uma moldura no preview de visualização', () => {
   beforeEach(() => {
     cy.viewport(1366, 768);
     cy.visit('/');
     cy.reload();
-  })
+  });
 
-  it("Será validado se o elemento que serve de container para a imagem tem a cor de fundo branca", () => {
-    cy.get('#meme-image-container').should($container => {
+  it('Será validado se o elemento que serve de container para a imagem tem a cor de fundo branca', () => {
+    cy.get('#meme-image-container').should(($container) => {
       expect($container).to.have.css('background-color', 'rgb(255, 255, 255)');
     });
-  })
+  });
 
-  it("Será validado se o elemento que serve de container para a imagem tem uma borda preta, sólida, com 1 pixel de largura", () => {
+  it('Será validado se o elemento que serve de container para a imagem tem uma borda preta, sólida, com 1 pixel de largura', () => {
     cy.get('#meme-image-container').should(
       'have.css',
       'border',
-      '1px solid rgb(0, 0, 0)',
+      '1px solid rgb(0, 0, 0)'
     );
-  })
+  });
 
-  it("Será validado se a imagem deve estar totalmente contida dentro do elemento identificado como `meme-image-container`", () => {
+  it('Será validado se a imagem deve estar totalmente contida dentro do elemento identificado como `meme-image-container`', () => {
     memeUpload();
     checkFullOverlappage('#meme-image-container', '#meme-image');
-  })
+  });
 });
 
-describe("4 - Adicione o texto que será inserido sobre a imagem", () => {
+describe('4 - Adicione o texto que será inserido sobre a imagem', () => {
   beforeEach(() => {
     cy.viewport(1366, 768);
     cy.visit('/');
     cy.reload();
-  })
+  });
 
-  it("Será validado se o texto do elemento `meme-text` dtem uma sombra preta, de 5 pixels na horizontal, 5 pixels na vertical e um raio de desfoque de 5 pixels", () => {
-    cy.get('#meme-text').should($text => {
+  it('Será validado se o texto do elemento `meme-text` dtem uma sombra preta, de 5 pixels na horizontal, 5 pixels na vertical e um raio de desfoque de 5 pixels', () => {
+    cy.get('#meme-text').should(($text) => {
       expect($text).to.have.css('text-shadow', 'rgb(0, 0, 0) 5px 5px 5px');
     });
-  })
+  });
 
-  it("Será validado se o texto do elemento `meme-text` tem a fonte com o tamanho de 30 pixels", () => {
-    cy.get('#meme-text').should('have.css','font-size','30px',
-    );
-  })
+  it('Será validado se o texto do elemento `meme-text` tem a fonte com o tamanho de 30 pixels', () => {
+    cy.get('#meme-text').should('have.css', 'font-size', '30px');
+  });
 
-  it("Será validado se o texto do elemento `meme-text` deve estar na cor branca", () => {
-    cy.get('#meme-text').should(
-      'have.css',
-      'color',
-      'rgb(255, 255, 255)',
-    );
-  })
+  it('Será validado se o texto do elemento `meme-text` deve estar na cor branca', () => {
+    cy.get('#meme-text').should('have.css', 'color', 'rgb(255, 255, 255)');
+  });
 });
 
-describe("5 - Limite o tamanho do texto do meme", () => {
+describe('5 - Limite o tamanho do texto do meme', () => {
   beforeEach(() => {
     cy.viewport(1366, 768);
     cy.visit('/');
     cy.reload();
-  })
+  });
 
-  it("Será validado se a quantidade máxima de caracteres digitáveis no elemento `text-input` deve ser 60.", () => {
+  it('Será validado se a quantidade máxima de caracteres digitáveis no elemento `text-input` deve ser 60.', () => {
     cy.get('#text-input')
       .type('I have written a line that has precisely sixty-one characters')
       .should(
         'have.value',
-        'I have written a line that has precisely sixty-one character',
+        'I have written a line that has precisely sixty-one character'
       );
-  })
+  });
 });
 
 // Bonus requirements
 
-describe("6 - Crie três botões que alterem as cores da borda dos memes", () => {
+describe('6 - Crie três botões que alterem as cores da borda dos memes', () => {
   beforeEach(() => {
     cy.viewport(1366, 768);
     cy.visit('/');
-  })
+  });
 
-  it("Será validado se o botão com identificado com id `fire` funciona corretamente.", () => {
-    cy.get('#fire').should(
-      'have.css',
-      'background-color',
-      'rgb(255, 0, 0)',
-    );
+  it('Será validado se o botão com identificado com id `fire` funciona corretamente.', () => {
+    cy.get('#fire').should('have.css', 'background-color', 'rgb(255, 0, 0)');
     cy.get('#fire').click();
     cy.get('#meme-image-container').should(
       'have.css',
       'border',
-      '3px dashed rgb(255, 0, 0)',
+      '3px dashed rgb(255, 0, 0)'
     );
-  })
+  });
 
-  it("Será validado se o botão com identificado com id `water` funciona corretamente.", () => {
-    cy.get('#water').should(
-      'have.css',
-      'background-color',
-      'rgb(0, 0, 255)',
-    );
+  it('Será validado se o botão com identificado com id `water` funciona corretamente.', () => {
+    cy.get('#water').should('have.css', 'background-color', 'rgb(0, 0, 255)');
     cy.get('#water').click();
     cy.get('#meme-image-container').should(
       'have.css',
       'border',
-      '5px double rgb(0, 0, 255)',
+      '5px double rgb(0, 0, 255)'
     );
-  })
+  });
 
-  it("Será validado se o botão com identificado com id `earth` funciona corretamente.", () => {
-    cy.get('#earth').should(
-      'have.css',
-      'background-color',
-      'rgb(0, 128, 0)',
-    );
+  it('Será validado se o botão com identificado com id `earth` funciona corretamente.', () => {
+    cy.get('#earth').should('have.css', 'background-color', 'rgb(0, 128, 0)');
     cy.get('#earth').click();
     cy.get('#meme-image-container').should(
       'have.css',
       'border',
-      '6px groove rgb(0, 128, 0)',
+      '6px groove rgb(0, 128, 0)'
     );
-  })
+  });
 });
 
-describe("7 - Crie um conjunto de quatro imagens pré prontas que permita que a pessoa usuária possa escolher", () => {
+describe('7 - Crie um conjunto de quatro imagens pré prontas que permita que a pessoa usuária possa escolher', () => {
   beforeEach(() => {
     cy.viewport(1366, 768);
     cy.visit('/');
-  })
+  });
 
-  it("Será validado se as imagens prontas apresentam o comportamente esperado.", () => {
-    [1, 2, 3, 4].map(memeId => {
+  it('Será validado se as imagens prontas apresentam o comportamente esperado.', () => {
+    [1, 2, 3, 4].map((memeId) => {
       cy.get(`#meme-${memeId}`).click();
       cy.get('#meme-image')
         .should('have.attr', 'src')
         .and('match', new RegExp(`imgs/meme${memeId}.png$`));
     });
-  })
+  });
 });
--- script.js
+++ script.js
@@ -1,7 +1,79 @@
 const inputImg = document.getElementById('meme-insert');
 const imgContainer = document.getElementById('meme-image');
+const input = document.getElementById('text-input');
+const text = document.getElementById('meme-text');
+const container = document.getElementById('meme-image-container');
+const meme1 = document.getElementById('meme-1');
+const meme2 = document.getElementById('meme-2');
+const meme3 = document.getElementById('meme-3');
+const meme4 = document.getElementById('meme-4');
 
-inputImg.addEventListener('change', () => {
+const upImage = () => {
   const file = inputImg.files[0];
-  imgContainer.src = URL.createObjectURL(file);
+  imgContainer.style.backgroundImage = `url(${URL.createObjectURL(file)})`;
+  imgContainer.style.backgroundSize = 'cover';
+  imgContainer.style.backgroundPosition = 'center';
+  imgContainer.style.backgroundRepeat = 'no-repeat';
+};
+inputImg.addEventListener('change', upImage);
+
+input.addEventListener('change', () => {
+  const texto = input.value;
+  text.innerText = texto.toUpperCase();
+  text.style.fontFamily = '"Alfa Slab One", serif';
+  text.style.fontWeight = '400';
+  text.style.fontStyle = 'normal';
+  text.style.color = 'yellow';
+});
+
+const fire = document.getElementById('fire');
+const water = document.getElementById('water');
+const earth = document.getElementById('earth');
+
+fire.style.backgroundColor = 'rgb(255, 0, 0)';
+fire.addEventListener('click', () => {
+  container.style.border = '3px dashed rgb(255, 0, 0)';
+});
+
+water.style.backgroundColor = 'rgb(0, 0, 255)';
+water.addEventListener('click', () => {
+  container.style.border = '5px double rgb(0, 0, 255)';
+});
+
+earth.style.backgroundColor = 'rgb(0, 128, 0)';
+earth.addEventListener('click', () => {
+  container.style.border = '6px groove rgb(0, 128, 0)';
+});
+
+meme1.style.backgroundImage = `url(${'imgs/meme1.png'})`;
+meme2.style.backgroundImage = `url(${'imgs/meme2.png'})`;
+meme3.style.backgroundImage = `url(${'imgs/meme3.png'})`;
+meme4.style.backgroundImage = `url(${'imgs/meme4.png'})`;
+
+meme1.addEventListener('click', () => {
+  imgContainer.style.backgroundImage = `url(${'imgs/meme1.png'})`;
+  imgContainer.style.backgroundSize = 'cover';
+  imgContainer.style.backgroundPosition = 'center';
+  imgContainer.style.backgroundRepeat = 'no-repeat';
+});
+
+meme2.addEventListener('click', () => {
+  imgContainer.style.backgroundImage = `url(${'imgs/meme2.png'})`;
+  imgContainer.style.backgroundSize = 'cover';
+  imgContainer.style.backgroundPosition = 'center';
+  imgContainer.style.backgroundRepeat = 'no-repeat';
+});
+
+meme3.addEventListener('click', () => {
+  imgContainer.style.backgroundImage = `url(${'imgs/meme3.png'})`;
+  imgContainer.style.backgroundSize = 'cover';
+  imgContainer.style.backgroundPosition = 'center';
+  imgContainer.style.backgroundRepeat = 'no-repeat';
+});
+
+meme4.addEventListener('click', () => {
+  imgContainer.style.backgroundImage = `url(${'imgs/meme4.png'})`;
+  imgContainer.style.backgroundSize = 'cover';
+  imgContainer.style.backgroundPosition = 'center';
+  imgContainer.style.backgroundRepeat = 'no-repeat';
 });
